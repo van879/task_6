f8c1dce9ed390d66e11a6ce4503c7770baaeda77
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 01:12:49 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace task_6
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int pictures = 52;
+			int picturesInRow = 3;
+			int rows = pictures / picturesInRow;
+			int remains = pictures % picturesInRow;
+			Console.WriteLine($"полных рядов - {rows}; останется картинок - {remains}");
+		}
+	}
+}
